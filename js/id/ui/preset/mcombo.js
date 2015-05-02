8d2a0e4ef94af9b01110e8274a6b30bc565985be
Thomas Kalka <thomas.kalka@gmail.com>
2015-05-02 11:45:26 +0200

    switch to a pure d3 mcombo box

--- js/id/ui/preset/mcombo.js
+++ js/id/ui/preset/mcombo.js
@@ -7,20 +7,20 @@ iD.ui.preset.typeMCombo = function(field, context) {
         input;
 
     function mcombo(selection) {
-        // console.log("!mcombo");
-        input = selection.selectAll('select').data([0]);
+        var combobox = d3.mcombobox();
+
+        input = selection.selectAll('input')
+            .data([0]);
 
         var enter = input.enter()
-            .append('select')
-            .attr('multiple','multiple')
+            .append('input')
+            .attr('type', 'text')
             .attr('id', 'preset-input-' + field.id);
-
-        var combobox = $("#preset-input-"+field.id);
 
         if (optstrings) { enter.attr('readonly', 'readonly'); }
 
         input
-            //.call(combobox)
+            .call(combobox)
             .on('change', change)
             .on('blur', change)
             .each(function() {
@@ -47,78 +47,45 @@ iD.ui.preset.typeMCombo = function(field, context) {
             });
 
         function stringsLoaded() {
-            // console.log("!stringsLoaded");
             var keys = _.keys(strings),
                 strs = [],
                 placeholders;
 
-            var data=keys.map(function(k) {
+            combobox.data(keys.map(function(k) {
                 var s = strings[k],
                     o = {};
                 o.title = o.value = s;
                 if (s.length < 20) { strs.push(s); }
                 return o;
-            });
+            }));
 
             placeholders = strs.length > 1 ? strs : keys;
-           
-            $.each(keys, function (i, o) {
-                combobox.append("<option>" + o + "</option>");
-            });
-
-            /* 
-            console.log("cbox",combobox);      
-            console.log("keys",keys);
-            console.log("data",data);
-            */
-
-            combobox.on("blur",function() {
-                console.log("combobox blur");
-                change();
-            }).on("change",function() {
-                console.log("combobox changed");
-                change();
-            });
-
-
-            var display_text = 
-                field.placeholder() || 
-                (placeholders.slice(0, 3).join(', ') + '...');
-
-
-            combobox.SumoSelect({ 
-                placeholder : display_text,
-                csvDispCount : 0 
-            });
-
-            // input.attr('placeholder', display_text );
-            //$(".SlectBox .placeholder").text( display_text );
-
+            input.attr('placeholder', field.placeholder() ||
+                (placeholders.slice(0, 3).join(', ') + '...'));
         }
+    }
 
-        function change() {
-            console.log("!change");
-            var optstring = _.find(_.keys(strings), function(k) { return strings[k] === input.value(); }),
-                value = 
-                    optstring || (combobox
-                        .val()
-                        .map(function(s) { return s.trim(); })
-                        .join(';')
-                        .replace(/\s+/g, '_'));
+    function change() {
+        var optstring = _.find(_.keys(strings), function(k) { return strings[k] === input.value(); }),
+            value = optstring || (input.value()
+                .split(';')
+                .map(function(s) { return s.trim(); })
+                .join(';')
+                .replace(/\s+/g, '_'));
+ 
+        console.log("!chage",value);
 
-            // if (field.type === 'typeMCombo' && !value) value = 'yes';
+        if (field.type === 'typeMCombo' && !value) value = 'yes';
 
-            var t = {};
-            t[field.key] = value || undefined;
-            event.change(t);
-        }
-
+        var t = {};
+        t[field.key] = value || undefined;
+        event.change(t);
     }
 
     mcombo.tags = function(tags) {
         var key = tags[field.key],
             value = strings[key] || key || '';
-        if (field.type === 'typeCombo' && value.toLowerCase() === 'yes') value = '';
+        if (field.type === 'typeMCombo' && value.toLowerCase() === 'yes') value = '';
         input.value(value);
     };
 
